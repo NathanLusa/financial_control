51a8af801a688200f5c530ce6c7cb3e9277500f9
Nathan Marcos Lusa <nathanlusa@MacBook-Pro.local>
2020-06-19 16:30:54 -0300

    DatePicker initial

---
+++ financial_control/static/js/datepicker.js
@@ -0,0 +1,9 @@
+export function DatePicker(element) {
+  const calendar = element.querySelector('.calendar')
+
+
+
+}
+
+const datepicker = document.querySelector('div.datepicker')
+DatePicker(datepicker)
